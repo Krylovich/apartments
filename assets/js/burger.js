33c8fb41cd1034af9bd6a6f026e4d21d0c82ae02
Krylovich <mariya.wings@gmail.com>
2023-01-19 21:49:02 +0300

    добавлены скрипты для адаптива

---
+++ assets/js/burger.js
@@ -0,0 +1,46 @@
+let navTog = document.querySelector('#navTog');
+let nav = document.querySelector('#header');
+let burger = document.querySelector('#burger');
+let logo = document.querySelector('#logo')
+
+
+    const toggleBurger = () => {
+
+        nav.classList.toggle('show');
+
+        document.body.classList.toggle('no-scroll');
+
+        burger.classList.toggle('open');
+    }; 
+
+        const hideNav = () => {
+
+            nav.classList.remove('show');
+
+            document.body.classList.remove('no-scroll');
+
+            document.getElementById('navTog').checked = false;
+
+            burger.classList.remove('open');
+        }
+
+        navTog.addEventListener('click', () => {
+
+          toggleBurger();
+
+        });
+
+        document.querySelectorAll(".nav-link").forEach(element => {
+
+           element.addEventListener("click", hideNav);
+
+        }); 
+
+       logo.addEventListener("click", hideNav);
+ 
+
+
+
+        
+
+        
---
+++ assets/js/sliderRev.js
@@ -0,0 +1,117 @@
+
+if (document.documentElement.clientWidth > 650) {
+/* Устанавливаем индекс слайда по умолчанию */
+let slideIndex = 1;
+let dotIndex = 1;
+showSlides(slideIndex);
+activeDots(dotIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
+function nextSlide() {
+    showSlides(slideIndex += 1);
+    activeDots(dotIndex += 1);
+}
+
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+    activeDots(dotIndex -= 1);  
+}
+
+/* Устанавливаем текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    activeDots(activeDots = n);
+}
+
+
+/* Функция перелистывания */
+function showSlides(n) {
+  let slides = document.getElementsByClassName("reviews-slide-item");
+    
+  if (n > slides.length) {
+    slideIndex = 1
+  }else if (n < 1) {
+    slideIndex = slides.length
+  }
+  
+  /* Проходим по каждому слайду в цикле for */
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }   
+  slides[slideIndex - 1].style.display = "flex"; 
+
+}
+
+function activeDots(n) {
+  let dots = document.getElementsByClassName("dot-square");
+
+  if (n > dots.length) {
+    dotIndex = 1
+  }else if (n < 1) {
+    dotIndex = dots.length
+  }
+
+  for (let dot of dots) {
+    dot.classList.remove('dot-active');
+  }
+  dots[dotIndex - 1].classList.add('dot-active');
+}
+} else {
+
+/* Устанавливаем индекс слайда по умолчанию */
+let slideIndex = 1;
+let dotIndex = 1;
+showSlides(slideIndex);
+activeDots(dotIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
+function nextSlide() {
+    showSlides(slideIndex += 1);
+    activeDots(dotIndex += 1);
+}
+
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+    activeDots(dotIndex -= 1);  
+}
+
+/* Устанавливаем текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    activeDots(activeDots = n);
+}
+
+    function showSlides(n) {
+        let slides = document.getElementsByClassName("reviews-slide-item-mob");
+          
+        if (n > slides.length) {
+          slideIndex = 1
+        }else if (n < 1) {
+          slideIndex = slides.length
+        }
+        
+        /* Проходим по каждому слайду в цикле for */
+        for (let slide of slides) {
+          slide.style.display = "none";
+        }   
+        slides[slideIndex - 1].style.display = "block"; 
+      
+      }
+      
+      function activeDots(n) {
+        let dots = document.getElementsByClassName("dot");
+      
+        if (n > dots.length) {
+          dotIndex = 1
+        }else if (n < 1) {
+          dotIndex = dots.length
+        }
+      
+        for (let dot of dots) {
+          dot.classList.remove('dot-active');
+        }
+        dots[dotIndex - 1].classList.add('dot-active');
+    }
+  }
