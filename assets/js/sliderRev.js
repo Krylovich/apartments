1e65226894133f0e2903621dea73f809c4fac293
Krylovich <mariya.wings@gmail.com>
2023-01-20 14:42:54 +0300

    добавлены правки

--- assets/js/sliderRev.js
+++ assets/js/sliderRev.js
@@ -1,64 +1,3 @@
-
-if (document.documentElement.clientWidth > 650) {
-/* Устанавливаем индекс слайда по умолчанию */
-let slideIndex = 1;
-let dotIndex = 1;
-showSlides(slideIndex);
-activeDots(dotIndex);
-
-/* Увеличиваем индекс на 1 — показываем следующий слайд*/
-function nextSlide() {
-    showSlides(slideIndex += 1);
-    activeDots(dotIndex += 1);
-}
-
-/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-    activeDots(dotIndex -= 1);  
-}
-
-/* Устанавливаем текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-    activeDots(activeDots = n);
-}
-
-
-/* Функция перелистывания */
-function showSlides(n) {
-  let slides = document.getElementsByClassName("reviews-slide-item");
-    
-  if (n > slides.length) {
-    slideIndex = 1
-  }else if (n < 1) {
-    slideIndex = slides.length
-  }
-  
-  /* Проходим по каждому слайду в цикле for */
-  for (let slide of slides) {
-    slide.style.display = "none";
-  }   
-  slides[slideIndex - 1].style.display = "flex"; 
-
-}
-
-function activeDots(n) {
-  let dots = document.getElementsByClassName("dot-square");
-
-  if (n > dots.length) {
-    dotIndex = 1
-  }else if (n < 1) {
-    dotIndex = dots.length
-  }
-
-  for (let dot of dots) {
-    dot.classList.remove('dot-active');
-  }
-  dots[dotIndex - 1].classList.add('dot-active');
-}
-} else {
-
 /* Устанавливаем индекс слайда по умолчанию */
 let slideIndex = 1;
 let dotIndex = 1;
@@ -84,12 +23,12 @@ function currentSlide(n) {
 }
 
     function showSlides(n) {
-        let slides = document.getElementsByClassName("reviews-slide-item-mob");
+        let slides = document.getElementsByClassName("reviews-slide-item");
           
         if (n > slides.length) {
           slideIndex = 1
         }else if (n < 1) {
-          slideIndex = slides.length
+          slideIndex = slidesMob.length
         }
         
         /* Проходим по каждому слайду в цикле for */
@@ -114,4 +53,4 @@ function currentSlide(n) {
         }
         dots[dotIndex - 1].classList.add('dot-active');
     }
-  }
+  
